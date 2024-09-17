989dd36e3270887ba20cf681102f83cbafd953de
Tobias O <tobias.olenyi@tum.de>
2024-09-17 10:58:27 +0200

    Add error page

---
+++ frontend/src/lib/status_emojis.ts
@@ -0,0 +1,41 @@
+export const emojis = {
+	// 1xx Informational
+	100: '💬', // Continue
+	101: '🔄', // Switching Protocols
+	102: '⏳', // Processing
+
+	// 2xx Success
+	200: '✅', // OK
+	201: '🆕', // Created
+	202: '👌', // Accepted
+	204: '🗑️', // No Content
+	206: '✂️', // Partial Content
+
+	// 3xx Redirection
+	300: '🔀', // Multiple Choices
+	301: '🏡', // Moved Permanently
+	302: '↪️', // Found (Previously "Moved temporarily")
+	304: '📜', // Not Modified
+	307: '⏭️', // Temporary Redirect
+	308: '🔄', // Permanent Redirect
+
+	// 4xx Client Errors
+	400: '❓', // Bad Request
+	401: '🔒', // Unauthorized
+	403: '🚫', // Forbidden
+	404: '🕵️', // Not Found
+	405: '🚷', // Method Not Allowed
+	408: '⏱️', // Request Timeout
+	418: '🫖', // I'm a teapot
+	420: '🫠', // Enhance Your Calm (Twitter) / Method Failure (Spring Framework)
+	429: '🚦', // Too Many Requests
+
+	// 5xx Server Errors
+	500: '💥', // Internal Server Error
+	501: '🚧', // Not Implemented
+	502: '🚪', // Bad Gateway
+	503: '🔧', // Service Unavailable
+	504: '⏳', // Gateway Timeout
+	507: '💾', // Insufficient Storage
+	508: '➿', // Loop Detected
+  };
---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<!--
+ Copyright 2024 Tobias Olenyi.
+ SPDX-License-Identifier: Apache-2.0
+-->
+
+<script>
+    import { emojis } from '$lib/status_emojis.ts';
+    import { page } from '$app/stores';
+</script>
+
+<div class="w-full text-token grid grid-cols-1 gap-4 h-screen place-items-center">
+    <div class="card bg-initial flex justify-center items-center">
+        <header class="card-header justify-center text-9xl" >{emojis[$page.status] ?? emojis[500]}</header>
+        <div class="p-4 space-y-4">
+            <h3>Error {$page.status} - Oops! Something went wrong.</h3>
+            <p class='pre'>{$page.error.message}</p>
+        </div>
+    </div>
+</div>
