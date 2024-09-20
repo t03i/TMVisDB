948ac7d4dad08c663ed68a53df4e735dffb352bb
Tobias O <tobias.olenyi@tum.de>
2024-09-20 18:00:39 +0200

    Refactor DataLoader

--- frontend/src/lib/components/DataLoader.svelte
+++ frontend/src/lib/components/DataLoader.svelte
@@ -10,6 +10,7 @@
     createGetProteinsByClade,
     createGetProteinsBySuperKingdom,
   } from "$lib/client/tMVis";
+
   import type { Topology } from "$lib/client/model";
   import {
     useQueryClient,
@@ -17,18 +18,32 @@
   } from "@tanstack/svelte-query";
 
   export let params: Record<string, string>;
+  export let initialData: any = [];
+  export let page_size: number = 20;
 
   let query: CreateQueryResult;
   $: {
+    console.log(initialData);
+    const queryParams = {
+      topology: params.topology as Topology,
+      has_signal_peptide: params.peptide === "true",
+      sequence_length_min: parseInt(params.min),
+      sequence_length_max: parseInt(params.max),
+      page_size: page_size,
+      page: parseInt(params.page) || 1,
+    };
+
+    const queryOptions = {
+      initialData: { data: initialData },
+      refetchOnMount: initialData.length === 0,
+    };
+
     if (!params.search_for) {
-      query = createGetRandomProteins(20);
+      query = createGetRandomProteins(20, { query: queryOptions });
     } else if (params.search_for === "id") {
       const organismId = parseInt(params.organism_id);
-      query = createGetProteinsByOrganism(organismId, {
-        topology: params.topology as Topology,
-        has_signal_peptide: params.peptide === "true",
-        sequence_length_min: parseInt(params.min),
-        sequence_length_max: parseInt(params.max),
+      query = createGetProteinsByOrganism(organismId, queryParams, {
+        query: queryOptions,
       });
     } else if (params.search_for === "taxa") {
       if (params.domain) {
@@ -40,13 +55,8 @@
             | "unclassified sequences",
           // @ts-ignore
           params.kingdom,
-
-          {
-            topology: params.topology as Topology,
-            has_signal_peptide: params.peptide === "true",
-            sequence_length_min: parseInt(params.min),
-            sequence_length_max: parseInt(params.max),
-          },
+          queryParams,
+          { query: queryOptions },
         );
       } else {
         query = createGetProteinsBySuperKingdom(
@@ -55,12 +65,8 @@
             | "Eukaryota"
             | "Bacteria"
             | "unclassified sequences",
-          {
-            topology: params.topology as Topology,
-            has_signal_peptide: params.peptide === "true",
-            sequence_length_min: parseInt(params.min),
-            sequence_length_max: parseInt(params.max),
-          },
+          queryParams,
+          { query: queryOptions },
         );
       }
     }
@@ -68,7 +74,8 @@
 </script>
 
 <slot
-  proteins={$query?.data ?? []}
+  isSuccessful={$query?.isSuccess}
   isLoading={$query?.isLoading}
   error={$query?.error}
+  data={$query?.data?.data}
 />
