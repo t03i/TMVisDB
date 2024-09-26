745a1a7b6981e322c99afd493473802d50386711
Tobias O <tobias.olenyi@tum.de>
2024-09-26 18:56:36 +0200

    Enable Situational Button activation

--- frontend/src/lib/components/FilterForm.svelte
+++ frontend/src/lib/components/FilterForm.svelte
@@ -9,37 +9,35 @@
 
   type FilterType = "taxa" | "id";
 
+  // Define default values in a dictionary
+  const defaultFilters = {
+    filterType: "taxa" as FilterType,
+    filterTopology: Topology.All,
+    filterMinLength: config.MIN_PROTEIN_LENGTH,
+    filterMaxLength: config.MAX_PROTEIN_LENGTH,
+    filterSignalPeptide: true,
+    filterOrganismId: "",
+    filterDomain: "",
+    filterKingdom: "",
+  };
+
   // Initialize filters with default values
-  let filterType: FilterType = "taxa";
-  let filterTopology: Topology = Topology.All;
-  let filterMinLength = config.MIN_PROTEIN_LENGTH;
-  let filterMaxLength = config.MAX_PROTEIN_LENGTH;
-  let filterSignalPeptide = true;
-  let filterOrganismId = "";
-  let filterDomain = "";
-  let filterKingdom = "";
+  $: filters = { ...defaultFilters };
 
   // Compute available clades based on selected domain
   $: availableClades =
-    taxonomyData.find((d) => d.value === filterDomain)?.clades || [];
+    taxonomyData.find((d) => d.value === filters.filterDomain)?.clades || [];
 
   // Ensure signal peptide filter is enabled when topology is 'All'
-  $: if (filterTopology === Topology.All) {
-    filterSignalPeptide = true;
+  $: if (filters.filterTopology === Topology.All) {
+    filters.filterSignalPeptide = true;
   }
 
   const dispatch = createEventDispatcher();
 
   // Reset filters to default values
   function resetFilters() {
-    filterType = "taxa";
-    filterTopology = Topology.All;
-    filterMinLength = config.MIN_PROTEIN_LENGTH;
-    filterMaxLength = config.MAX_PROTEIN_LENGTH;
-    filterSignalPeptide = true;
-    filterOrganismId = "";
-    filterDomain = "";
-    filterKingdom = "";
+    filters = { ...defaultFilters };
   }
 
   // Handle form reset
@@ -54,18 +52,18 @@
     event.preventDefault();
 
     const params: Record<string, string> = {
-      search_for: filterType,
-      topology: filterTopology,
-      peptide: String(filterSignalPeptide),
-      min: String(filterMinLength),
-      max: String(filterMaxLength),
+      search_for: filters.filterType,
+      topology: filters.filterTopology,
+      peptide: String(filters.filterSignalPeptide),
+      min: String(filters.filterMinLength),
+      max: String(filters.filterMaxLength),
     };
 
-    if (filterType === "id") {
-      params.organism_id = filterOrganismId;
+    if (filters.filterType === "id") {
+      params.organism_id = filters.filterOrganismId;
     } else {
-      params.domain = filterDomain;
-      params.kingdom = filterKingdom;
+      params.domain = filters.filterDomain;
+      params.kingdom = filters.filterKingdom;
     }
 
     goto(`?${new URLSearchParams(params).toString()}`);
@@ -77,19 +75,44 @@
     const url = $page.url;
     const params = new URLSearchParams(url.search);
 
-    filterType = (params.get("search_for") as FilterType) || "taxa";
-    filterTopology = (params.get("topology") as Topology) || Topology.All;
-    filterMinLength = parseInt(
-      params.get("min") || String(config.MIN_PROTEIN_LENGTH),
-    );
-    filterMaxLength = parseInt(
-      params.get("max") || String(config.MAX_PROTEIN_LENGTH),
-    );
-    filterSignalPeptide = params.get("peptide") !== "false";
-    filterOrganismId = params.get("organism_id") || "";
-    filterDomain = params.get("domain") || "";
-    filterKingdom = params.get("kingdom") || "";
+    Object.keys(defaultFilters).forEach((key) => {
+      const paramKey = key.replace("filter", "").toLowerCase();
+      const paramValue = params.get(paramKey);
+      if (paramValue !== null) {
+        if (key === "filterTopology") {
+          filters[key] = paramValue as Topology;
+        } else if (key === "filterMinLength" || key === "filterMaxLength") {
+          filters[key] = parseInt(paramValue);
+        } else if (key === "filterSignalPeptide") {
+          filters[key] = paramValue !== "false";
+        } else {
+          filters[key] = paramValue;
+        }
+      }
+    });
   });
+
+  // Check if current state matches default state
+  function isDefaultState(): boolean {
+    return Object.keys(defaultFilters).every(
+      (key) => filters[key] === defaultFilters[key],
+    );
+  }
+
+  // Check if a filter has been applied
+  function isFilterApplied(): boolean {
+    const url = $page.url;
+    const params = new URLSearchParams(url.search);
+    return params.has("search_for");
+  }
+  let canApplyFilter = false;
+  let canReset = false;
+  $: if (filters) {
+    canApplyFilter = !isDefaultState();
+  }
+  $: if (filters) {
+    canReset = isFilterApplied();
+  }
 </script>
 
 <form class="space-y-6" on:submit|preventDefault={handleSubmit}>
@@ -97,16 +120,16 @@
   <fieldset>
     <legend class="text-sm font-bold">Search By</legend>
     <RadioGroup class="flex space-x-4">
-      <RadioItem name="filter-1" bind:group={filterType} value="taxa"
+      <RadioItem name="filter-1" bind:group={filters.filterType} value="taxa"
         >Taxonomy</RadioItem
       >
-      <RadioItem name="filter-2" bind:group={filterType} value="id"
+      <RadioItem name="filter-2" bind:group={filters.filterType} value="id"
         >Organism ID</RadioItem
       >
     </RadioGroup>
   </fieldset>
 
-  {#if filterType === "id"}
+  {#if filters.filterType === "id"}
     <!-- Organism ID Input -->
     <div>
       <label for="organism_id" class="text-sm font-bold"
@@ -117,7 +140,7 @@
         type="number"
         class="input py-2 px-3 leading-tight"
         placeholder="Enter Organism ID"
-        bind:value={filterOrganismId}
+        bind:value={filters.filterOrganismId}
         required
       />
     </div>
@@ -128,7 +151,7 @@
       <select
         id="domain"
         class="select py-2 px-3 leading-tight"
-        bind:value={filterDomain}
+        bind:value={filters.filterDomain}
         required
       >
         <option value="" disabled selected>Select Domain</option>
@@ -145,7 +168,7 @@
         id="kingdom"
         size="4"
         class="select py-2 px-3 leading-tight"
-        bind:value={filterKingdom}
+        bind:value={filters.filterKingdom}
       >
         <option value="">All</option>
         {#each availableClades as value}
@@ -162,22 +185,22 @@
       <RadioGroup class="grid grid-cols-2 gap-4">
         <RadioItem
           name="topology-1"
-          bind:group={filterTopology}
+          bind:group={filters.filterTopology}
           value={Topology.All}>Non-filtered</RadioItem
         >
         <RadioItem
           name="topology-2"
-          bind:group={filterTopology}
+          bind:group={filters.filterTopology}
           value={Topology.Both}>Both Helix and Barrel</RadioItem
         >
         <RadioItem
           name="topology-3"
-          bind:group={filterTopology}
+          bind:group={filters.filterTopology}
           value={Topology["Alpha-helix"]}>Only Alpha-Helices</RadioItem
         >
         <RadioItem
           name="topology-4"
-          bind:group={filterTopology}
+          bind:group={filters.filterTopology}
           value={Topology["Beta-strand"]}>Only Beta-Barrels</RadioItem
         >
       </RadioGroup>
@@ -188,8 +211,8 @@
   <div class="flex items-center space-x-2">
     <SlideToggle
       name="signal-peptide"
-      bind:checked={filterSignalPeptide}
-      disabled={filterTopology === Topology.All}
+      bind:checked={filters.filterSignalPeptide}
+      disabled={filters.filterTopology === Topology.All}
     >
       Show sequences with signal peptides
     </SlideToggle>
@@ -204,17 +227,17 @@
       <input
         type="number"
         class="input"
-        bind:value={filterMinLength}
+        bind:value={filters.filterMinLength}
         min={config.MIN_PROTEIN_LENGTH}
-        max={filterMaxLength}
+        max={filters.filterMaxLength}
         placeholder="Min Length"
       />
       <span>to</span>
       <input
         type="number"
         class="input"
-        bind:value={filterMaxLength}
-        min={filterMinLength}
+        bind:value={filters.filterMaxLength}
+        min={filters.filterMinLength}
         max={config.MAX_PROTEIN_LENGTH}
         placeholder="Max Length"
       />
@@ -223,8 +246,15 @@
 
   <!-- Form Actions -->
   <div class="space-x-4">
-    <button type="submit" class="btn variant-filled">Apply</button>
-    <button type="button" on:click={handleReset} class="btn variant-soft">
+    <button type="submit" class="btn variant-filled" disabled={!canApplyFilter}
+      >Apply</button
+    >
+    <button
+      type="button"
+      on:click={handleReset}
+      class="btn variant-soft"
+      disabled={!canReset}
+    >
       Reset (Random)
     </button>
   </div>
