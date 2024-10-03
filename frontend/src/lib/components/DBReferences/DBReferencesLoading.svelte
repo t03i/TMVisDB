e2ea1627969f900ec24fa99b3db987233232a7d7
Tobias O <tobias.olenyi@tum.de>
2024-10-03 15:00:04 +0200

    Refactor ReferencesComponent

---
+++ frontend/src/lib/components/DBReferences/DBReferencesLoading.svelte
@@ -0,0 +1,26 @@
+<!--
+ Copyright 2024 Tobias Olenyi.
+ SPDX-License-Identifier: Apache-2.0
+-->
+
+<!--
+ Copyright 2024 Tobias Olenyi.
+ SPDX-License-Identifier: Apache-2.0
+-->
+<script lang="ts">
+  import { SOURCE_DATABASES } from "$lib/annotations";
+</script>
+
+<div {...$$restProps}>
+  <h3 class="h3 no-wrap">
+    <h3 class="h3 no-wrap">Annotation Sources</h3>
+  </h3>
+  <div class="flex flex-col lg:flex-row gap-4 justify-items-start mt-4">
+    {#each SOURCE_DATABASES as dbName}
+      <div class="flex mt-1 items-center">
+        <span class="mr-2">{dbName} Annotation:</span>
+        <span class="placeholder-circle w-16" />
+      </div>
+    {/each}
+  </div>
+</div>
---
+++ frontend/src/lib/components/DBReferences/index.ts
@@ -0,0 +1,5 @@
+// Copyright 2024 Tobias Olenyi.
+// SPDX-License-Identifier: Apache-2.0
+
+export { default as DBReferencesView } from './DBReferencesView.svelte';
+export { default as DBReferencesLoading } from './DBReferencesLoading.svelte';
