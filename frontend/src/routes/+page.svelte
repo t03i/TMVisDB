a1f85ef165ed1c913ef39a67f50146f2119b72e9
Tobias O <tobias.olenyi@tum.de>
2024-11-06 12:20:23 +0100

    docs(frontend): remove hardcoded name from landing page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -11,8 +11,9 @@
   <h1 class="h1">About {config.APP_NAME}</h1>
 
   <p>
-    <strong>{config.APP_NAME}</strong> provides per-residue transmembrane topology
-    annotations for proteins predicted as transmembrane proteins from the
+    <strong>{config.APP_NAME}</strong> provides per-residue transmembrane
+    topology annotations for proteins predicted as transmembrane proteins from
+    the
     <a href="https://doi.org/10.1093/nar/gkab1061" class="anchor"
       >AlphaFold DB</a
     >. The annotations are predicted with
@@ -26,8 +27,8 @@
   <div>
     <h2 class="h3">How to browse {config.APP_NAME}:</h2>
     <p>
-      To browse the predicted transmembrane proteins in {config.APP_NAME} via a table, you
-      can show a random selection or use the following filters:
+      To browse the predicted transmembrane proteins in {config.APP_NAME} via a table,
+      you can show a random selection or use the following filters:
     </p>
     <ul class="list-inside list-disc">
       <li>Transmembrane topology (alpha-helix, beta-strand)</li>
@@ -47,8 +48,8 @@
     <h2 class="h3">How to visualize predicted transmembrane proteins:</h2>
     <p>
       Single proteins of {config.APP_NAME} can be selected for 3D-visualization of
-      per-residue transmembrane topology annotation. You can either select a
-      protein from the table you generated while browsing {config.APP_NAME}, or you can
+      per-residue transmembrane topology annotation. You can either select a protein
+      from the table you generated while browsing {config.APP_NAME}, or you can
       directly enter a UniProt Identifier. The AlphaFold 2 structures of a
       protein are then shown with the corresponding color code of the predicted
       topology. You may also select the pLDDT score of AlphaFold 2 as a color
