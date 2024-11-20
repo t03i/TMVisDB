6ca3433d76e925313751f44e91db67af69fad294
Tobias O <tobias.olenyi@tum.de>
2024-11-20 18:46:35 +0100

    build: add sentry config

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -3,7 +3,7 @@
 
 import axios from 'axios';
 
-import { PUBLIC_API_URL, PUBLIC_PROJECT_NAME, PUBLIC_GITHUB_REPO,PUBLIC_MIN_PROTEIN_LENGTH, PUBLIC_MAX_PROTEIN_LENGTH, PUBLIC_MAINTENANCE_MODE } from '$env/static/public';
+import { PUBLIC_API_URL, PUBLIC_PROJECT_NAME, PUBLIC_GITHUB_REPO,PUBLIC_MIN_PROTEIN_LENGTH, PUBLIC_MAX_PROTEIN_LENGTH, PUBLIC_MAINTENANCE_MODE, PUBLIC_SENTRY_DSN_FRONTEND } from '$env/static/public';
 
 import { GitHubLinks } from './github';
 
@@ -14,6 +14,7 @@ interface Config {
     API_BASE_URL: string;
     APP_NAME: string;
     MAINTENANCE_MODE: boolean;
+    SENTRY_DSN: string;
 }
 
 
@@ -24,7 +25,8 @@ const config: Config = {
     GITHUB_LINKS: new GitHubLinks(PUBLIC_GITHUB_REPO || {owner:'t03i', name: 'TMvisDB'}),
     API_BASE_URL: PUBLIC_API_URL || 'http://localhost:8000',
     APP_NAME: PUBLIC_PROJECT_NAME || 'TMVisDB',
-    MAINTENANCE_MODE: PUBLIC_MAINTENANCE_MODE.toLowerCase() === 'true' || false
+    MAINTENANCE_MODE: PUBLIC_MAINTENANCE_MODE.toLowerCase() === 'true' || false,
+    SENTRY_DSN: PUBLIC_SENTRY_DSN_FRONTEND
 };
 axios.defaults.baseURL = config.API_BASE_URL;
 // TODO: fix this for server side rendering
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -10,6 +10,26 @@
   import { storePopup } from "@skeletonlabs/skeleton";
   storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
+  import config from "$lib/config";
+
+  import Sentry from @sentry/browser
+  Sentry.init({
+  dsn: config.SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
   import "../app.css";
 
   import {
@@ -18,7 +38,6 @@
     Navigation,
     QueryLayout,
   } from "$lib/components/Layout";
-  import config from "$lib/config";
   import Maintenance from "$lib/components/Maintenance.svelte";
   import PrivacyBanner from "$lib/components/PrivacyBanner.svelte";
 </script>
