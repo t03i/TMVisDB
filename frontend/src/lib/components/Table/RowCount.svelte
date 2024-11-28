3395eed1e1fea58f18acca0f0f58b7452537883d
Tobias O <tobias.olenyi@tum.de>
2024-11-28 12:16:58 +0100

    refactor(frontend): add data-based row count

--- frontend/src/lib/components/Table/RowCount.svelte
+++ frontend/src/lib/components/Table/RowCount.svelte
@@ -3,23 +3,26 @@
   import type { CreateQueryResult } from "@tanstack/svelte-query";
 
   export let currentPage: number;
+  export let loadedRows: number;
   export let pageSize: number;
-  export let countQuery: CreateQueryResult<ProteinCount, HTTPValidationError> | null;
+  export let countQuery: CreateQueryResult<
+    ProteinCount,
+    HTTPValidationError
+  > | null;
 
   $: startRow = (currentPage - 1) * pageSize + 1;
-  $: endRow = currentPage * pageSize;
-  // #todo Fix actual end item
+  $: endRow = loadedRows + startRow - 1;
 </script>
 
 <aside class="mr-6 text-sm leading-8">
-    <b>{startRow}</b>
-    - <b>{endRow}</b>
-    {#if countQuery}
+  <b>{startRow}</b>
+  - <b>{endRow}</b>
+  {#if countQuery}
     \
     {#if $countQuery?.isLoading}
-      <span class='italic animate-pulse'>Loading ... </span>
+      <span class="animate-pulse italic">Loading ... </span>
     {:else if $countQuery?.isError}
-      <span class='italic text-error-500'>Error loading count</span>
+      <span class="italic text-error-500">Error loading count</span>
     {:else}
       <b>{$countQuery?.data?.count}</b>
     {/if}
