2e84aac41f8b216a3fffe44cb6e494f4c5076a27
Tobias O <tobias.olenyi@tum.de>
2025-01-29 19:18:54 +0100

    feat: add FAQ section

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,8 @@
   import config from "$lib/config";
   // @ts-ignore
   import overview from "$lib/assets/overview.svg";
+  import { DiscussionCategory } from "$lib/github";
+  import FAQ from "$lib/components/FAQ.svelte";
 </script>
 
 <svelte:head>
@@ -24,48 +26,31 @@
 
   <!-- TODO improve overview image-->
   <div class="flex place-content-center">
-    <img src={overview} alt="Overview of {config.APP_NAME}" class="md:w-2/3 w-full" />
+    <img
+      src={overview}
+      alt="Overview of {config.APP_NAME}"
+      class="w-full md:w-2/3"
+    />
   </div>
 
-  <div>
-    <h2 class="h3">How to browse {config.APP_NAME}:</h2>
-    <p>
-      To browse the predicted transmembrane proteins in {config.APP_NAME} via a table,
-      you can show a random selection or use the following filters:
-    </p>
-    <ul class="list-inside list-disc">
-      <li>Transmembrane topology (alpha-helix, beta-strand)</li>
-      <li>Include/Exclude sequences with predicted signal peptides</li>
-      <li>Taxonomy (UniProt Organism Identifier, Domain, Kingdom)</li>
-      <li>Protein length</li>
-    </ul>
-    <p class="mt-2 text-sm italic">
-      Note: We follow the general length restrictions of AlphaFold DB: a minimum
-      of 16 amino acids for all organisms, and a maximum of 1,280 amino acids
-      for all organisms except SwissProt (2,700 amino acids) and human (no
-      restrictions).
-    </p>
-  </div>
-
-  <div>
-    <h2 class="h3">How to visualize predicted transmembrane proteins:</h2>
-    <p>
-      Single proteins of {config.APP_NAME} can be selected for 3D-visualization of
-      per-residue transmembrane topology annotation. You can either select a protein
-      from the table you generated while browsing {config.APP_NAME}, or you can
-      directly enter a UniProt Identifier. The AlphaFold 2 structures of a
-      protein are then shown with the corresponding color code of the predicted
-      topology. You may also select the pLDDT score of AlphaFold 2 as a color
-      code.
-    </p>
-  </div>
-
-  <div class="flex space-x-4">
+  <div class="mt-4 flex space-x-4">
     <a type="button" class="variant-filled-primary btn" href="filter">
       Start Browsing
     </a>
     <a type="button" class="variant-filled-secondary btn" href="/search">
       Search Protein
     </a>
+    <a
+      type="button"
+      class="variant-filled-tertiary btn"
+      href={config.GITHUB_LINKS.getDiscussionsUrl(DiscussionCategory.QA)}
+    >
+      Ask a Question
+    </a>
+  </div>
+
+  <div class="card my-4 p-4">
+    <h2 class="h3 mb-4">Frequently Asked Questions</h2>
+    <FAQ />
   </div>
 </div>
